23cced4908603e10c3e3c605a67b1266d60d975a
Jesus Ramirez <jesus.r.n.17@gmail.com>
2019-06-10 22:52:16 -0500

    build method in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
@@ -7,15 +8,29 @@ const log = require('gulplog');
 const babelify = require('babelify');
 const watchify = require('watchify');
 const del = require('del');
-
+const cssnano = require('cssnano');
+const useref = require('gulp-useref');
+const postcss = require('gulp-postcss');
+const gulpif = require('gulp-if');
 const server = require('browser-sync').create();
 
 const paths = {
   html: {
-    src: 'src/*.html'
+    src: 'src/*.html',
+    buildDest: 'build'
+  },
+  sass: {
+    src: 'src/sass/**/*.scss',
+    dest: 'src/styles'
   },
   scripts: {
-    src: 'src/js/index.js'
+    src: 'src/js/index.js',
+    dest: 'src/js/dist',
+    buildDest: 'build/js/dist'
+  },
+  fonts: {
+    src: 'src/fonts/**/*',
+    builDest: 'build/fonts'
   }
 };
 
@@ -33,13 +48,14 @@ const bundler = watchify(browserify(bundlerOptions));
 
 bundler.on('update', makeDevBundle);
 
+//// dev functions
 function makeDevBundle() {
   return bundler
     .bundle()
     .on('error', log.error)
     .pipe(source('bundle.js'))
     .pipe(buffer())
-    .pipe(gulp.dest('src/js/dist'))
+    .pipe(gulp.dest(paths.scripts.dest))
     .pipe(server.stream({ once: true }));
 }
 
@@ -57,8 +73,68 @@ function reloadServer(done) {
   done();
 }
 
+function compileSass() {
+  return gulp
+    .src(paths.sass.src)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.sass.dest))
+    .pipe(server.stream());
+}
+
 function watch() {
+  gulp.watch(paths.sass.src, gulp.series(compileSass, reloadServer));
   gulp.watch(paths.html.src, { events: 'change' }, reloadServer);
 }
 
-exports.default = gulp.series(makeDevBundle, initServer, watch);
+//// build functions
+function makeBuildBundle() {
+  return bundler
+    .bundle()
+    .on('error', log.error)
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .on('error', log.error)
+    .pipe(gulp.dest(paths.scripts.buildDest))
+    .pipe(server.stream({ once: true }));
+}
+
+function clean() {
+  return del('build');
+}
+
+function fonts(done) {
+  gulp.src(paths.fonts.src).pipe(gulp.dest(paths.fonts.builDest));
+  done();
+}
+
+function buildCss() {
+  const plugins = [cssnano()];
+  return gulp
+    .src(paths.html.src)
+    .pipe(useref())
+    .pipe(gulpif('*.css', postcss(plugins)))
+    .pipe(gulp.dest(paths.html.buildDest));
+}
+
+function initBuildServer(done) {
+  server.init({
+    server: {
+      baseDir: 'build'
+    }
+  });
+  done();
+}
+
+exports.default = gulp.series(compileSass, makeDevBundle, initServer, watch);
+exports.build = gulp.series(clean, compileSass, buildCss, fonts, makeBuildBundle);
+
+//solo para probar el desempeño
+exports.buildAndTest = gulp.series(
+  clean,
+  compileSass,
+  buildCss,
+  fonts,
+  makeBuildBundle,
+  initBuildServer
+);
